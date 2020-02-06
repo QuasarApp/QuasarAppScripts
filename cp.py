f680b277968f6bbd966a7dcfe2014dbb343fefd2
a.yankovich <EndrIIMail@gmail.com>
2020-02-06 10:56:22 +0300

    added first scripts

---
+++ cp.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import shutil   
+
+src = sys.argv[1]
+dest = sys.argv[2]
+
+def recursive_overwrite(src, dest, ignore=None):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = os.listdir(src)
+        if ignore is not None:
+            ignored = ignore(src, files)
+        else:
+            ignored = set()
+        for f in files:
+            if f not in ignored:
+                recursive_overwrite(os.path.join(src, f), 
+                                    os.path.join(dest, f), 
+                                    ignore)
+    else:
+        shutil.copyfile(src, dest)
+        
+        
+recursive_overwrite(src, dest)
+
